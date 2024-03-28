b68d0f0501a1f75b9710639ffe596b00c52151c5
minseok <heekms6@naver.com>
2024-03-28 17:09:11 +0900

    html,script_수업

---
+++ web_front/javascript/ex29.js
@@ -0,0 +1,20 @@
+testObj = {
+    key : '값',
+    key1 : '값1',
+    test : () => {
+        alert('new test'); 
+    } ,
+    test1 : () => {
+        alert('new test111'); 
+    },
+    num : 123
+}
+/*
+let num = 123;
+const test = () => {
+    alert('test'); 
+}
+const test1 = () => {
+    alert('test111'); 
+}
+*/
